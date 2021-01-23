44dfea8545c298493bd2016ebeccb6690edf3225
Sam <samblackburn2828@gmail.com>
2021-01-23 17:51:10 +0000

    end of day

--- skyrim-quest-tracker/src/App.js
+++ skyrim-quest-tracker/src/App.js
@@ -1,10 +1,12 @@
 import "./App.css";
 import Header from "./Header.js";
+import Questlist from "./Questlist.js";
 
 function App() {
   return (
     <div className="App">
       <Header />
+      <Questlist />
     </div>
   );
 }
---
+++ skyrim-quest-tracker/src/Questlist.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const Questlist = () => {
+  return <h1>Quests go here</h1>;
+};
+
+export default Questlist;
