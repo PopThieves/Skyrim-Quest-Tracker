1bc6653f46db7b8efeaaa2c2f1b9c577f5c4edea
Sam <samblackburn2828@gmail.com>
2021-01-25 16:16:17 +0000

    questlist functionality added

--- skyrim-quest-tracker/src/App.js
+++ skyrim-quest-tracker/src/App.js
@@ -1,14 +1,50 @@
+import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Questlist from "./Questlist.js";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Questlist />
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    mainQuestsInfo: {
+      title: "Main QuestLine",
+      mainQuests: [
+        "Unbound",
+        "Before the Storm",
+        "Bleak Falls Barrow",
+        "Dragon Rising",
+        "The Way of The Voice",
+        "The Horn of Jurgen Windcaller",
+      ],
+      mainQuestDesc: [
+        "Escape Helgen",
+        "Warn the Jarl of Whiterun",
+        "Retrieve the Dragonstone",
+        "Slay your first Dragon",
+        "Meet with the Greybeards",
+        "Recover the Horn from Ustengrav",
+      ],
+    },
+    thievesGuild: ["Find Brynyolf"],
+    thievesGuildDesc: ["Find Brynyolf"],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <div className="questcontainer">
+          <Questlist
+            title={this.state.mainQuestsInfo.title}
+            mainQuests={this.state.mainQuestsInfo.mainQuests}
+            mainQuestDesc={this.state.mainQuestsInfo.mainQuestDesc}
+          />
+          <Questlist
+            mainQuests={this.state.thievesGuild}
+            mainQuestDesc={this.state.thievesGuildDesc}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
--- skyrim-quest-tracker/src/Questlist.js
+++ skyrim-quest-tracker/src/Questlist.js
@@ -1,7 +1,25 @@
 import React from "react";
 
-const Questlist = () => {
-  return <h1>Quests go here</h1>;
+const Questlist = (props) => {
+  const { title } = props;
+  const { mainQuestDesc } = props;
+  return (
+    <div className="list">
+      <p className="questType">{title}</p>
+      <ul>
+        {props.mainQuests.map((quest, i) => {
+          return (
+            <div className="questlist">
+              <li key={quest} className="quest">
+                <p className="questName">{quest}</p>
+                <p className="questDesc">{mainQuestDesc[i]}</p>
+              </li>
+            </div>
+          );
+        })}
+      </ul>
+    </div>
+  );
 };
 
 export default Questlist;
