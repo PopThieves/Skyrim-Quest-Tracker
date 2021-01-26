b539091366eca636240126a8f5e89e8a15fe620d
Sam <samblackburn2828@gmail.com>
2021-01-26 17:15:23 +0000

    started data for quests

--- skyrim-quest-tracker/src/App.js
+++ skyrim-quest-tracker/src/App.js
@@ -2,30 +2,19 @@ import React from "react";
 import "./App.css";
 import Header from "./Header.js";
 import Questlist from "./Questlist.js";
+import {
+  mainQuestsInfo,
+  darkBrotherhoodQuestsInfo,
+  collegeWinterholdQuestsInfo,
+  companionsQuestsInfo,
+} from "./data";
 
 class App extends React.Component {
   state = {
-    mainQuestsInfo: {
-      title: "Main QuestLine",
-      mainQuests: [
-        "Unbound",
-        "Before the Storm",
-        "Bleak Falls Barrow",
-        "Dragon Rising",
-        "The Way of The Voice",
-        "The Horn of Jurgen Windcaller",
-      ],
-      mainQuestDesc: [
-        "Escape Helgen",
-        "Warn the Jarl of Whiterun",
-        "Retrieve the Dragonstone",
-        "Slay your first Dragon",
-        "Meet with the Greybeards",
-        "Recover the Horn from Ustengrav",
-      ],
-    },
-    thievesGuild: ["Find Brynyolf"],
-    thievesGuildDesc: ["Find Brynyolf"],
+    mainQuestsInfo: mainQuestsInfo,
+    darkBrotherhoodQuestsInfo: darkBrotherhoodQuestsInfo,
+    collegeWinterholdQuestsInfo: collegeWinterholdQuestsInfo,
+    companionsQuestsInfo: companionsQuestsInfo,
   };
   render() {
     return (
@@ -34,12 +23,23 @@ class App extends React.Component {
         <div className="questcontainer">
           <Questlist
             title={this.state.mainQuestsInfo.title}
-            mainQuests={this.state.mainQuestsInfo.mainQuests}
-            mainQuestDesc={this.state.mainQuestsInfo.mainQuestDesc}
+            mainQuests={this.state.mainQuestsInfo.quests}
+            mainQuestDesc={this.state.mainQuestsInfo.questDesc}
           />
           <Questlist
-            mainQuests={this.state.thievesGuild}
-            mainQuestDesc={this.state.thievesGuildDesc}
+            title={this.state.darkBrotherhoodQuestsInfo.title}
+            mainQuests={this.state.darkBrotherhoodQuestsInfo.quests}
+            mainQuestDesc={this.state.darkBrotherhoodQuestsInfo.questDesc}
+          />
+          <Questlist
+            title={this.state.collegeWinterholdQuestsInfo.title}
+            mainQuests={this.state.collegeWinterholdQuestsInfo.quests}
+            mainQuestDesc={this.state.collegeWinterholdQuestsInfo.questDesc}
+          />
+          <Questlist
+            title={this.state.companionsQuestsInfo.title}
+            mainQuests={this.state.companionsQuestsInfo.quests}
+            mainQuestDesc={this.state.companionsQuestsInfo.questDesc}
           />
         </div>
       </div>
